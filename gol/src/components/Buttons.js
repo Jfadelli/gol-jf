b9ea2c11903417fc947227514283a48a8fa98860
Jason <47641725+Jfadelli@users.noreply.github.com>
2020-09-24 12:51:03 -0700

    mvp.2

---
+++ gol/src/components/Buttons.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { ButtonToolbar, Dropdown, DropdownButton } from 'react-bootstrap';
+
+export default class Buttons extends React.Component {
+
+	handleSelect = (evt) => {
+		this.props.gridSize(evt);
+
+	}
+	handleSpeedSelect = (evt) => {
+		this.props.playSpeed(evt);
+	}
+
+	render() {
+		return (
+			<div className="center">
+				<ButtonToolbar>
+					<button className="btn btn-default" onClick={this.props.playButton}>
+						Play
+					</button>
+					<button className="btn btn-default" onClick={this.props.pauseButton}>
+					  Pause
+					</button>
+					<button className="btn btn-default" onClick={this.props.clear}>
+					  Clear
+					</button> 
+					<button className="btn btn-default" onClick={this.props.seed}>
+					  Seed
+					</button>
+          			<button className="btn btn-default" onClick={this.props.stepForward}>
+					  Step Forward
+					</button>
+			<DropdownButton id="dropdown-item-button" title="Speed">
+				<Dropdown.Item className="btn btn-secondary" onSelect={this.handleSpeedSelect} eventKey="10">Fast</Dropdown.Item>
+            	<Dropdown.Item className="btn btn-secondary" onSelect={this.handleSpeedSelect} eventKey="11">Normal</Dropdown.Item>
+            	<Dropdown.Item className="btn btn-secondary" onSelect={this.handleSpeedSelect} eventKey="12">Slow</Dropdown.Item>
+				<Dropdown.Item className="btn btn-secondary" onSelect={this.handleSpeedSelect} eventKey="13">Super Slow</Dropdown.Item>
+			</DropdownButton>
+
+
+          <DropdownButton id="dropdown-item-button" title="Size">
+            <Dropdown.Item className="btn btn-secondary" onSelect={this.handleSelect} eventKey="1">20x10</Dropdown.Item>
+            <Dropdown.Item className="btn btn-secondary" onSelect={this.handleSelect} eventKey="2">50x30</Dropdown.Item>
+            <Dropdown.Item className="btn btn-secondary" onSelect={this.handleSelect} eventKey="3">70x50</Dropdown.Item>
+          </DropdownButton>
+				</ButtonToolbar>
+			</div>
+			)
+	}
+}
